cffa1e86f13b9d0dc1623f073217cb98413eab2a
gkspranger <greg@spranger.us>
2023-05-11 11:09:40 -0400

    stubbing out create

--- main.go
+++ main.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+// type cmdArgs struct {
+// 	workspace string
+// 	variable  string
+// 	value     string
+// }
+
+func create() {
+	createCmd := flag.NewFlagSet("create", flag.ExitOnError)
+	workspacePtr := createCmd.String("w", "", "TFC workspace name")
+	namePtr := createCmd.String("n", "", "TFC workspace variable name")
+	valuePtr := createCmd.String("v", "", "TFC workspace variable value")
+	createCmd.Parse(os.Args[2:])
+	output := "workspace: " + *workspacePtr + "\n" +
+		"variable: " + *namePtr + "\n" +
+		"value: " + *valuePtr + "\n"
+	fmt.Print(output)
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	if len(os.Args) < 2 {
+		fmt.Println("missing command")
+		os.Exit(1)
+	}
+
+	switch os.Args[1] {
+	case "create":
+		create()
+	default:
+		fmt.Println("invalid command")
+	}
 }
